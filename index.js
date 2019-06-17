54a6866f993affeff04db2a281558dfe6e388116
Developer <>
2019-06-17 12:40:37 +0300

    everything except home screen

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-import { rootReducer } from "./redux/ducks";
-import { rootSaga } from "./redux/ducks";
+import { rootReducer, rootSaga } from "./src/redux";
+import { name as appName } from "./app.json";
+import App from "./src/App";
 
 const sagaMiddleware = createSagaMiddleware();
 
